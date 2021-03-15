be9d7ebec2efaaa50f2f8cef583ae87cf6cce3c1
Chris Moon <ozarwork@gmail.com>
2021-03-15 20:47:41 +0900

    Solved 1

---
+++ 1. Two Sum (Easy)/p1_2.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+		subtract = {}
+		for i in range(len(nums)):
+			if nums[i] in subtract:
+				return [i, subtract[nums[i]]]
+			subtract[target - nums[i]] = i
